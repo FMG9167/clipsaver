2f9b3eff4b55be0e40ce52d7658ab42833013485
FMG <funtasticaarav@gmail.com>
2025-08-13 13:59:32 +0530

    pyperclip works now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import pyperclip as ppc
-# import json
+from memory_profiler import profile
 import tkinter.font as font
 
 @profile
@@ -16,8 +16,8 @@ def func():
     def getClip(number):
         with open(filename, "r") as f:
             l = f.read().split("\n")
-            if number <= len(l)-1 and number > 0:
-                return l[number-1]
+            if number <= len(l):
+                return l[number]
             else:
                 return "EOF"
 
@@ -29,7 +29,8 @@ def func():
         with open(filename, "r") as f:
             a = f.read().split("\n")[0:-1]
             for i in range(len(a)):
-                yield str(i+1)+ "\t\t\t" + a[i]
+                yield str(i+1)+ "       " + a[i]
+
     def clear():
         with open(filename, "w") as f:
             f.write()
@@ -38,16 +39,16 @@ def func():
         ppc.copy(getClip(Lb1.curselection()[0]))
 
     def efunc():
-        with open(filename, "r") as f:
-            a = f.read().count("\n")
         b= getBoard()
         c=1
+        d = Lb1.get(0,Lb1.size()+1)
         for i in b:
-            Lb1.insert(c, i)
+            if (c,i) not in d:
+                Lb1.insert(c,i)
             c+=1
-        Lb1.grid(row=1,column=0)
+        if Lb1.get(0,Lb1.size()+1) != tuple():
+            Lb1.activate(0)
         r.after(2000,efunc)
-
     
     r=Tk()
     r.title("ClipSaver")
@@ -59,6 +60,7 @@ def func():
     B2 = Button(f, text = "Copy Selection", command=copy, font = s)
     B3 = Button(f, text = "Clear Clipboard", command=clear, font = s)
     Lb1 = Listbox(r, height=50, width=100, selectmode="SINGLE")
+    Lb1.grid(row=1,column=0)
 
 
     B1.grid(column=0,row=0,padx=10)
@@ -74,22 +76,3 @@ def func():
 
 if __name__ == "__main__":
     func()
-
-
-# root = Tk(baseName = "ClipSaver")
-# root.title("ClipSaver")
-# S25 = font.Font(family="Arial", size=25)
-
-# L1 = Label(root, text="ClipSaver is working", font=S25)
-# B1 = Button(root, text="exit", command=root.destroy, font=S25)
-# I1 = Entry(root, font=S25)
-
-# clips=[]
-
-
-# L1.grid(row=0,column=2, columnspan=100)
-# B1.grid(row=0,column=0)
-# I1.grid(row=1,column=2,columnspan=100)
-
-# root.mainloop()
-
