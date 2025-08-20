574bfdfb960338423468e66dc308a53e495a69f5
FMG <funtasticaarav@gmail.com>
2025-08-20 19:04:18 +0530

    not much done. sorted the code. Removed the Move buttons and added the category lists

--- main.py
+++ main.py
@@ -10,32 +10,13 @@ from datetime import datetime
 
 menu = 0
 prev = 0
-latest = 0
+latest = ""
 
 @profile
 def main():
     mydb = sqlite3.connect('clipsaver.db')
     cursor = mydb.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS tables(tableName VARCHAR(50), type INTEGER)')
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS unclassified(dnt DATETIME NOT NULL, '
-        'clip VARCHAR(65535))')
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS personal(dnt DATETIME NOT NULL, '
-        'clip VARCHAR(65535))')
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS work(dnt DATETIME NOT NULL, '
-        'clip VARCHAR(65535))')
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS passwords(dnt DATETIME NOT NULL, '
-        'clip VARCHAR(65535))')
-    mydb.commit()
-
     global menu, prev, latest
-
-    menu = 0
-    prev = 0
-    latest = ""
     delay = 50
 
     ppc.copy(latest)
@@ -53,84 +34,16 @@ def main():
         return None
 
     def getCurrentTable():
-        return getTable(0)
+        return MenuListBox.curselection()[0]
 
     def getClip(ListIndex):
         clip = MainListBox.get(ListIndex, ListIndex)
         return clip[0]
 
-    def writeClip(clip,num):
-        dnt = time.strftime("%Y-%m-%d %H:%M:%S")
-        numb = cursor.execute("SELECT count(*) FROM {}".format(getTable(num))).fetchone()[0]
-        if numb>=50:
-            cursor.execute("DELETE FROM {} WHERE dnt=?".format(getTable(num)),(cursor.execute("SELECT dnt FROM {} ORDER BY dnt DESC".format(getTable(num))).fetchone()[0],))
-            MainListBox.delete(0,0)
-
-        cursor.execute("INSERT INTO {} VALUES(?,?)".format(getTable(num)),(dnt,clip))
-        mydb.commit()
-
-    def getBoard(num):
-        records = cursor.execute("SELECT count(*) FROM {}".format(getTable(num))).fetchone()[0]
-        query = cursor.execute("SELECT clip FROM {}".format(getTable(num)))
-        for i in range(records if records <= 50 else 50):
-            yield query.fetchone()[0]   # memory optimization technique
-
-    def updateList(num):
-        newClip =  cursor.execute("SELECT clip FROM {} ORDER BY dnt DESC".format(getTable(num))).fetchone()[0]
-        MainListBox.insert(MainListBox.size(),newClip)
-
-    def clear(num):
-        cursor.execute("DELETE FROM {}".format(getTable(num)))
-        mydb.commit()
-        MainListBox.delete(0, MainListBox.size()+1)
-
     def copy():
         global latest
         latest = getClip(MainListBox.curselection()[0])
         ppc.copy(latest)
-
-    def delete():
-        global menu
-        clip = getClip(MainListBox.curselection()[0])
-        cursor.execute("DELETE FROM {} WHERE clip=?".format(getCurrentTable()), (clip,))
-        MainListBox.delete(MainListBox.curselection()[0])
-        mydb.commit()
-
-    def deleteOldPwd():
-        records = cursor.execute("SELECT count(*) FROM passwords").fetchone()[0]
-        query = cursor.execute("SELECT * FROM passwords")
-        delL = ""
-
-        for i in range(records):
-            result = query.fetchone()    # fetching rows one at a time instead of calling all the rows into memory
-            if "day" in str(datetime.now() - datetime.strptime(result[0], "%Y-%m-%d %H:%M:%S")):
-                delL+=str(result[0])
-
-        for i in delL:
-            cursor.execute("DELETE FROM passwords WHERE dnt=?", (str(i),))
-
-        if delL != "":
-            mydb.commit()
-
-    def mainLoop():
-        global menu, latest
-        current = ppc.paste()
-
-        if current != latest and (time.strftime("%Y-%m-%d %H:%M:%S"), current) not in cursor.execute("SELECT dnt, clip FROM {}".format(getCurrentTable())).fetchall():
-            writeClip(current,menu)
-            latest = current
-            updateList(menu)
-
-        if MainListBox.curselection() !=  ():
-            CopyButton.configure(state = "normal")
-            DeleteButton.configure(state = "normal")
-            updateMoveFrame()
-        else:
-            CopyButton.configure(state = "disabled")
-            DeleteButton.configure(state = "disabled")
-            updateMoveFrame(1)
-        deleteOldPwd()
-        root.after(delay,mainLoop)
 
     def changeMenu(num):
         global menu
@@ -158,105 +71,66 @@ def main():
                 PasswordsMenuButton.configure(state = "disabled")
         listRouter()
 
-    def changeList(num):
-        b = getBoard(num)
-        c = 1
-        d = MainListBox.get(0,MainListBox.size()+1)
-
-        for i in b:
-            if i not in d:
-                MainListBox.insert(c,i)
-            c += 1
-
     def updateMoveFrame(do = 0):
         global menu
         if do == 1:
-            UnclassifiedMoveButton.configure(state = "disabled")
-            PersonalMoveButton.configure(state = "disabled")
-            WorkMoveButton.configure(state = "disabled")
-            PasswordsMoveButton.configure(state = "disabled")
             MoveButton.configure(state = "disabled")
             return ""
 
         MoveButton.configure(state = "normal")
-        match menu:
-            case 0:
-                UnclassifiedMoveButton.configure(state = "disabled")
-                PersonalMoveButton.configure(state = "normal")
-                WorkMoveButton.configure(state = "normal")
-                PasswordsMoveButton.configure(state = "normal")
-            case 1:
-                UnclassifiedMoveButton.configure(state = "normal")
-                PersonalMoveButton.configure(state = "disabled")
-                WorkMoveButton.configure(state = "normal")
-                PasswordsMoveButton.configure(state = "normal")
-            case 2:
-                UnclassifiedMoveButton.configure(state = "normal")
-                PersonalMoveButton.configure(state = "normal")
-                WorkMoveButton.configure(state = "disabled")
-                PasswordsMoveButton.configure(state = "normal")
-            case 3:
-                UnclassifiedMoveButton.configure(state = "normal")
-                PersonalMoveButton.configure(state = "normal")
-                WorkMoveButton.configure(state = "normal")
-                PasswordsMoveButton.configure(state = "disabled")
         return None
 
+    #
+    #       Database Access
+    #
 
     def createList(tableName, tableType):
         cursor.execute("CREATE TABLE IF NOT EXISTS {} (dnt DATETIME NOT NULL, clip VARCHAR(65535))".format(tableName))
         cursor.execute("INSERT INTO tables VALUES (?,?)", (tableName,tableType))
+        mydb.commit()
 
-    def newList():
-        def submitList():
-            name = nameField.get()
-            pwd = "selected" in typePass.state()
-            udlWindow.destroy()
-            createList(name, pwd)
+    def tableGen(length):
+        query = cursor.execute("SELECT tablename FROM tables")
+        for i in range(length):
+            yield query.fetchone()[0]
 
-        udlWindow = Toplevel(mainWindow)
-        udlWindow.title("New List")
-        udlWindow.resizable(False, False)
-        udlWindow.configure(width=350, height=200)
-        mainFrame = Frame(udlWindow, padx=20, pady=20)
-        buttonFrame= Frame(mainFrame, padx=10, pady=10)
-        udlLabel = Label(mainFrame, text = "Create New List", padx=10, pady=10, font=("Arial", 20))
-        nameField = Entry(mainFrame, width = 30, justify = CENTER, name = "name", font=s)
-        typePass = ttk.Checkbutton(mainFrame, text = "Is this a password List?", variable=IntVar())
-        submitButton = Button(buttonFrame, text = "Submit", command = submitList, padx = 10, font=s)
-        cancelButton = Button(buttonFrame, text = "Cancel", command = udlWindow.destroy, padx = 10, font=s)
-
-        udlLabel.grid(row = 0, column = 0)
-        nameField.grid(row = 1, column = 0)
-        typePass.grid(row = 2, column = 0)
-        submitButton.grid(row = 0, column = 0)
-        cancelButton.grid(row = 0, column = 1)
-        buttonFrame.grid(row = 3, column = 0)
-
-        mainFrame.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-    def listRouter():
-        global menu, prev
-
-        if prev !=  menu:
-            MainListBox.delete(0,MainListBox.size()+1)
-            prev = menu
-
-        changeList(menu)
-
-    def moveTo(num):
+    def delete():
         global menu
-
         clip = getClip(MainListBox.curselection()[0])
-        cursor.execute("DELETE FROM {} WHERE clip = ?;".format(getCurrentTable()),(clip,))
-        writeClip(clip,num)
+        cursor.execute("DELETE FROM {} WHERE clip=?".format(getCurrentTable()), (clip,))
+        MainListBox.delete(MainListBox.curselection()[0])
+        mydb.commit()
 
+    def clear(num):
+        cursor.execute("DELETE FROM {}".format(getTable(num)))
+        mydb.commit()
+        MainListBox.delete(0, MainListBox.size()+1)
+
+    def writeClip(clip,num):
+        dnt = time.strftime("%Y-%m-%d %H:%M:%S")
+        numb = cursor.execute("SELECT count(*) FROM {}".format(getTable(num))).fetchone()[0]
+        if numb>=50:
+            cursor.execute("DELETE FROM {} WHERE dnt=?".format(getTable(num)),(cursor.execute("SELECT dnt FROM {} ORDER BY dnt DESC".format(getTable(num))).fetchone()[0],))
+            MainListBox.delete(0,0)
+
+        cursor.execute("INSERT INTO {} VALUES(?,?)".format(getTable(num)),(dnt,clip))
+        mydb.commit()
+
+    def getBoard(num):
+        records = cursor.execute("SELECT count(*) FROM {}".format(getTable(num))).fetchone()[0]
+        query = cursor.execute("SELECT clip FROM {}".format(getTable(num)))
+        for i in range(records if records <= 50 else 50):
+            yield query.fetchone()[0]   # memory optimization technique
 
     def moveClip(clip, newTable):
         cursor.execute("DELETE FROM {} WHERE clip=?;".format(getCurrentTable()),(clip,))
         MainListBox.delete(MainListBox.get(0,"end").index(clip))
         cursor.execute("INSERT INTO {} VALUES (?,?)".format(newTable),(time.strftime("%Y-%m-%d %H:%M:%S"),clip))
         mydb.commit()
+
+    #
+    #       App Windows
+    #
 
     def moveClipWindow(clip):
         moveWindow = Toplevel(mainWindow)
@@ -284,11 +158,119 @@ def main():
         buttonFrame.grid(row = 3, column = 0)
         mainFrame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
+    def newListWindow():
+        def submitList():
+            name = nameField.get()
+            pwd = "selected" in typePass.state()
+            udlWindow.destroy()
+            createList(name, pwd)
+
+        udlWindow = Toplevel(mainWindow)
+        udlWindow.title("New List")
+        udlWindow.resizable(False, False)
+        udlWindow.configure(width=350, height=200)
+        mainFrame = Frame(udlWindow, padx=20, pady=20)
+        buttonFrame= Frame(mainFrame, padx=10, pady=10)
+        udlLabel = Label(mainFrame, text = "Create New List", padx=10, pady=10, font=("Arial", 20))
+        nameField = Entry(mainFrame, width = 30, justify = CENTER, name = "name", font=s)
+        typePass = ttk.Checkbutton(mainFrame, text = "Is this a password List?", variable=IntVar())
+        submitButton = Button(buttonFrame, text = "Submit", command = submitList, padx = 10, font=s)
+        cancelButton = Button(buttonFrame, text = "Cancel", command = udlWindow.destroy, padx = 10, font=s)
+
+        udlLabel.grid(row = 0, column = 0)
+        nameField.grid(row = 1, column = 0)
+        typePass.grid(row = 2, column = 0)
+        submitButton.grid(row = 0, column = 0)
+        cancelButton.grid(row = 0, column = 1)
+        buttonFrame.grid(row = 3, column = 0)
+
+        mainFrame.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    #
+    #       EXITING THE APP
+    #
 
     def exiting():
         cursor.close()
         mydb.close()
         mainWindow.destroy()
+
+
+    #
+    #       App Algorithms
+    #
+
+    def init():
+        cursor.execute('CREATE TABLE IF NOT EXISTS tables(tableName VARCHAR(50), type INTEGER)')
+        mydb.commit()
+
+    def updateList(num):
+        newClip =  cursor.execute("SELECT clip FROM {} ORDER BY dnt DESC".format(getTable(num))).fetchone()[0]
+        MainListBox.insert(MainListBox.size(),newClip)
+
+    def deleteOldPwd():
+        records = cursor.execute("SELECT count(*) FROM passwords").fetchone()[0]
+        query = cursor.execute("SELECT * FROM passwords")
+        delL = ""
+
+        for i in range(records):
+            result = query.fetchone()    # fetching rows one at a time instead of calling all the rows into memory
+            if "day" in str(datetime.now() - datetime.strptime(result[0], "%Y-%m-%d %H:%M:%S")):
+                delL+=str(result[0])
+
+        for i in delL:
+            cursor.execute("DELETE FROM passwords WHERE dnt=?", (str(i),))
+
+        if delL != "":
+            mydb.commit()
+
+    def changeList(num):
+        b = getBoard(num)
+        c = 1
+        d = MainListBox.get(0,MainListBox.size()+1)
+
+        for i in b:
+            if i not in d:
+                MainListBox.insert(c,i)
+            c += 1
+
+    def mainLoop():
+        global menu, latest
+        current = ppc.paste()
+
+        if current != latest and (time.strftime("%Y-%m-%d %H:%M:%S"), current) not in cursor.execute("SELECT dnt, clip FROM {}".format(getCurrentTable())).fetchall():
+            writeClip(current,menu)
+            latest = current
+            updateList(menu)
+
+        if MainListBox.curselection() !=  ():
+            CopyButton.configure(state = "normal")
+            DeleteButton.configure(state = "normal")
+            updateMoveFrame()
+        else:
+            CopyButton.configure(state = "disabled")
+            DeleteButton.configure(state = "disabled")
+            updateMoveFrame(1)
+        deleteOldPwd()
+
+        tables = tableGen(cursor.execute("SELECT count(*) FROM tables").fetchone()[0])
+        for name in tables:
+            if name not in MenuListBox.get(0, "end"):
+                MenuListBox.insert(MenuListBox.size(), name)
+
+        root.after(delay,mainLoop)
+
+    def listRouter():
+        global menu, prev
+
+        if prev !=  menu:
+            MainListBox.delete(0,MainListBox.size()+1)
+            prev = menu
+
+        changeList(menu)
+    #
+    #       Main Window
+    #
 
     mainWindow = Tk()
     mainWindow.resizable(False, False)
@@ -314,21 +296,15 @@ def main():
     WorkMenuButton = Button(frameMenu, text = "Work", command = lambda: changeMenu(2), font = s)
     PasswordsMenuButton = Button(frameMenu, text = "Passwords", command = lambda: changeMenu(3), font = s)
 
-    MenuListBox = Listbox(frameMenu, height = 50, width = 50, selectmode = SINGLE, font = s)
+    MenuListBox = Listbox(frameMove, height = 10, width = 10, selectmode = SINGLE, font = s)
 
 
 
 
     MainListBox = Listbox(frameMiddle, height = 50, width = 100, selectmode = "SINGLE")
+    MainListBox.bind("<<ListboxSelect>>", listRouter)
 
 
-
-
-    MoveHeading = Label(frameMove, text = "Move Clip to:", font = s)
-    UnclassifiedMoveButton = Button(frameMove, text = "Unclassified", command = lambda: moveTo(0), font = s, state = "disabled")
-    PersonalMoveButton = Button(frameMove, text = "Personal", command = lambda: moveTo(1), font = s, state = "disabled")
-    WorkMoveButton = Button(frameMove, text = "Work", command = lambda: moveTo(2), font = s, state = "disabled")
-    PasswordsMoveButton = Button(frameMove, text = "Passwords", command = lambda: moveTo(3), font = s, state = "disabled")
 
 
     MoveButton = Button(frameMove, text = "Move Clip", command = lambda: moveClipWindow(MainListBox.get(MainListBox.curselection()[0])), font = s, state = "disabled")
@@ -339,18 +315,14 @@ def main():
     CopyButton = Button(frameActions, text = "Copy Selection", command = copy, font = s)
     DeleteButton = Button(frameActions, text = "Delete Selection", command = delete, font = s)
     ClearButton = Button(frameActions, text = "Clear Clipboard", command = lambda: clear(menu), font = s)
-    NewButton = Button(frameActions, text = "New List", command = newList, font = s)
+    NewButton = Button(frameActions, text = "New List", command = newListWindow, font = s)
 
     UnclassifiedMenuButton.grid(column = 0, row = 0, padx = 5)
     PersonalMenuButton.grid(column = 1, row = 0, padx = 5)
     WorkMenuButton.grid(column = 2, row = 0, padx = 5)
     PasswordsMenuButton.grid(column = 3, row = 0, padx = 5)
 
-    MoveHeading.grid(column = 0, row = 0, pady = 5)
-    UnclassifiedMoveButton.grid(column = 0, row = 1, pady = 5)
-    PersonalMoveButton.grid(column = 0, row = 2, pady = 5)
-    WorkMoveButton.grid(column = 0, row = 3, pady = 5)
-    PasswordsMoveButton.grid(column = 0, row = 4, pady = 5)
+    MenuListBox.grid(column = 0, row = 1, padx = 5)
     MoveButton.grid(column = 0, row = 5, pady = 5)
 
     CloseButton.grid(column = 0,row = 0,padx = 10)
@@ -368,6 +340,8 @@ def main():
 
     root.place(relx = 0.5, rely = 0.5, anchor = "center")
 
+
+    init()
     listRouter()
     mainLoop()
 
